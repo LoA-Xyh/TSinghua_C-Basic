b9a0ed682077213d8d0b15e57b379dfb37dfca28
Num9ber <gimasxyh@gmail.com>
2017-08-04 15:44:32 +0800

    题目：读入并显示整数

---
+++ Input and output number/Input and output number/Input and output number.cpp
@@ -0,0 +1,20 @@
+// Input and output number.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int radiusData;
+	cout << "Please enter the radius!\n";
+	cin >> radiusData;
+	cout << "The radius is:" << radiusData << '\n';
+	cout << "PI is:" << 3.14 << '\n';
+	cin >> radiusData;
+	cout << "Now the radius is changed to:" << radiusData << '\n';
+    return 0;
+}
+
