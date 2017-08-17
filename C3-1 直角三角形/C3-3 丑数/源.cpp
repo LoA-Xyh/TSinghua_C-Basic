3523fa7dc469d8de36db2590f8c2b56282e35877
Num9ber <gimasxyh@gmail.com>
2017-08-17 18:41:49 +0800

    C3-3

--- C3-1 直角三角形/C3-3 丑数/源.cpp
+++ C3-1 直角三角形/C3-3 丑数/源.cpp
@@ -2,16 +2,21 @@
 using namespace std;
 
 bool uglyNum(int n) {
-	while (n % 2 == 0)
-		n /= 2;
-	while (n % 3 == 0)
-		n /= 3;
-	while (n % 5 == 0)
-		n /= 5;
-	if (n == 1)
-		return true;
-	else 
+	if (n == 1) {
 		return false;
+	}
+	else {
+		while (n % 2 == 0)
+			n /= 2;
+		while (n % 3 == 0)
+			n /= 3;
+		while (n % 5 == 0)
+			n /= 5;
+		if (n == 1)
+			return true;
+		else
+			return false;
+	}
 }
 
 int main() {
